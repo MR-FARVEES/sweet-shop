72172a5c65fad740d1ce202aba4881c9a72df01d
jainoos <ammjainoos@gmail.com>
2025-04-28 00:47:55 +0530

    create user route

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 
+// Get user profile
 export const getUserProfile = async (req, res) => {
   try {
     const user = await User.findById(req.user._id).select("-password");
@@ -14,6 +15,7 @@ export const getUserProfile = async (req, res) => {
   }
 };
 
+// Update user profile
 export const updateUserProfile = async (req, res) => {
   const { firstName, lastName, email, address, contact, bussinessName } =
     req.body;
---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+import userController from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+//Get user profile
+router.get("/profile", userController.getUserProfile);
+
+//Update user profile
+router.put("/profile", userController.updateUserProfile);
